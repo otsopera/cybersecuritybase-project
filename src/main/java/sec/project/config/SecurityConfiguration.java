9f3d7cdb444453aa2666235bae11d0129e970e1c
otsopera <otsopera@users.noreply.github.com>
2017-01-29 15:42:35 +0200

    Disable csrf protection
    Disable csrf protection. Try csrf to delete all participants

--- src/main/java/sec/project/config/SecurityConfiguration.java
+++ src/main/java/sec/project/config/SecurityConfiguration.java
@@ -21,7 +21,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         
-        //http.csrf().disable();
+        http.csrf().disable();
         // no real security at the moment
         //http.authorizeRequests()
         //        .anyRequest().permitAll();
